3e85203f4705b1445ddbd8353654a50c7df312f7
Aline <aline.bsbsouza@gmail.com>
2022-08-18 14:58:38 +0100

    Create script.js
    estudos JS

---
+++ script.js
@@ -0,0 +1,118 @@
+//tipos primitivos
+
+//boolean
+/* var boo = false;
+console.log(typeof(boo));
+
+//number
+var num = 1;
+console.log(typeof(num));
+
+//string  cacartere
+ var nome = "Aline";
+console.log(typeof(nome));  */
+ 
+//declarando variável VAR
+//var nome = "Ebby";
+//console.log(nome);
+
+// LET
+//let nome01 = "Aly"
+//nome01 = "Baba"
+//console.log(nome01);
+
+//const
+//const constante = "Pav" ;
+//console.log(constante);
+
+//ESCOPO
+/* var escopoglobal = "Sou Global";
+console.log(escopoglobal);
+
+function escopolocal () {
+    let escopolocalinterno = "sou interno";
+    console.log(escopolocalinterno);
+}
+escopolocal(); */
+
+// atribuição
+
+//var atribuição = 1;
+
+// comparação
+
+//var comparacao = '0' == 0 ;
+//console.log(comparacao);
+
+
+//comparação identica
+//var comp_indentica = 0 === '0'
+//console.log (comp_indentica);
+
+
+//Operadores aritméticos
+
+//adição
+/*var adicao = 1+1 ;
+console.log( adicao);
+
+//subtração
+
+var sub = 3-3;
+console.log(sub);
+
+//multiplicação
+var multi = 2*3;
+console.log(multi);
+
+// divisão real /
+var divi = 6/2;
+console.log (divi);v*/
+
+//divisão interira ou resto da divisão
+
+/*var resto_divi = 5 % 2;
+console.log(resto_divi);
+
+//potenciação
+var potencia = 2**10;
+console.log(potencia);
+*/
+
+// maior ou igual que  >=
+/*var maior_que = 5>=5;
+console.log(maior_que);
+
+// menor que <
+var menor_que = 5<1;
+console.log(menor_que);
+
+// igual a ==
+var igual = 5==5;
+console.log(igual);
+
+// estritamente igual ===
+var estritamente_igual = 5==='5';
+console.log(estritamente_igual);
+
+*/
+
+
+
+
+//Operedores lógicos
+
+// Considera todos os valores verdadeiros             && e 
+/*var e = true && true;
+console.log(e);
+
+var ou = true || false ;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);*/
+
+// Considera qualquer valor verdadeiro               || ou 
+
+// Inverte o valor true para false e vice-versa      ! não
+
