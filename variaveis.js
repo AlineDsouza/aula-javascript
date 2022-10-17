d4a4a4cfbd11f7fd6c441c26764ddfaac0f9d75f
Aline <aline.bsbsouza@gmail.com>
2022-10-17 10:03:29 +0100

    Create variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,43 @@
+//let idade = 40
+//let msg = `Eu tenho ${idade} anos`
+//console.log(msg)
+//console.log(typeof msg, typeof idade)
+
+//const n1 = 10
+//const n2 = 2.1
+//console.log(`O tipo de n1 é ${typeof n1} e seu valor é ${n1}`)
+//console.log(`O tipo de n1 é ${typeof n2} e seu valor é ${n2}`)
+
+// Boleano true or false
+
+//const valido = true;
+//console.log(`Cadastro válido ${valido}`)
+
+//let varteste
+//console.log(typeof varteste)
+//varteste = 10
+//console.log(typeof varteste , varteste)
+
+// conversão entre tipos
+//let n1 = 10
+//let n2 = "2.122"
+//n2 = parseFloat(n2)
+//n2 = Number(n2)
+//console.log (n1 + n2)
+
+//converter numero em string
+
+//n2 = 10
+//n2 = n2.toString()
+//console.log(n2, typeof n2)
+
+// incremento e decremento
+//let i = 0
+//i++
+//console.log(i)
+
+//let idade = 18
+//let paisPresentes = true
+//bilhete = false
+//const podeViajar = (idade >= 18 || paisPresentes ) && bilhete
+//console.log(`Pode viajar ? ${podeViajar}`)
