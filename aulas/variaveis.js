3a39fc41c463921840082ad9613a9a13f570ae15
Aline Silva <aline.bsbsouza@gmail.com>
2022-11-02 16:26:56 +0000

    up

--- aulas/variaveis.js
+++ aulas/variaveis.js
@@ -41,3 +41,4 @@
 //bilhete = false
 //const podeViajar = (idade >= 18 || paisPresentes ) && bilhete
 //console.log(`Pode viajar ? ${podeViajar}`)
+
---
+++ exercicios/calculo.js
@@ -0,0 +1,17 @@
+
+/*Faça um programa paara calcular o valor uma viagem
+
+1 - Preço do combustível
+2 - Gasto médio de combustível do carro por km
+3 - Distancia em km da viagem
+
+imprima no console o valor que será gasto em combustível para realizae essa viagem*/
+
+const valorCombustivel = 5.79;
+const kmPorLitro = 10;
+const distanciaPercorrida = 100;
+
+const litrosConsumidos = distanciaPercorrida / kmPorLitro;
+const GastoDaViagem =  valorCombustivel * litrosConsumidos;
+
+console.log(`O gasto da viagem será de R$${GastoDaViagem.toFixed(2)}`)
