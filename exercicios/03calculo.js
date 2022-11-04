8dc18d4414547830237e0d3af7396fb02ae28095
Aline <aline.bsbsouza@gmail.com>
2022-11-04 16:49:40 +0000

    Create 03calculo.js

---
+++ exercicios/03calculo.js
@@ -0,0 +1,11 @@
+//Exercício
+/*
+1) Faça um algoritmo que dado 3 notas tiradas por um aluno em um semestre de faculdade calcule e imprima a sua média e a sua classificação conforme a tabela abaixo.
+
+Média = (nota 1 + nota 2 + nota 3 ) / 3;
+
+Classificação:
+- Média menor que 5, reprovado
+- Média entre 5 e 7, recuperação
+- Média maior que 7, aprovado
+*/
