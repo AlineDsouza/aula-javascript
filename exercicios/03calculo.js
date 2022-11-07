42966f83207783231f13de9e10222973afdb85c7
Aline Silva <aline.bsbsouza@gmail.com>
2022-11-07 10:05:14 +0000

    nwe

--- exercicios/03calculo.js
+++ exercicios/03calculo.js
@@ -9,3 +9,20 @@ Classificação:
 - Média entre 5 e 7, recuperação
 - Média maior que 7, aprovado
 */
+
+const nota1 = 5
+const nota2 = 15
+const nota3 = 1
+
+const mediaEscolar = (nota1 + nota2 + nota3) / 3
+
+if (mediaEscolar <5){
+    console.log(`Sua nota é ${mediaEscolar.toFixed(2)}, REPROVADO`)
+}else if(mediaEscolar>=5 && mediaEscolar<=7){
+    console.log(`Sua nota é ${mediaEscolar.toFixed(2)}, RECUPERAÇÃO`)
+}else if (mediaEscolar >7){
+    console.log(`Sua nota é ${mediaEscolar.toFixed(2)}, APROVADO`)
+}
+else{
+     console.log('nada aqui')
+}
