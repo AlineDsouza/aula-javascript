fce3b3382a1b9af9f5be70cb41da62ba46f0bed2
Aline Silva <aline.bsbsouza@gmail.com>
2022-11-14 15:07:45 +0000

    update

---
+++ aulas/04funcoes.js
@@ -0,0 +1,14 @@
+function escrevaSeuNome (nome){
+    console.log(`Seu nome é ${nome}`)
+}
+escrevaSeuNome('Zulema')
+escrevaSeuNome('Xena') 
+
+function verificarIdade(idade){
+    if (idade >=18){
+        console.log('Maior de idade')
+    }else{
+        console.log('Menor de idade')
+    }
+}
+verificarIdade(11)
