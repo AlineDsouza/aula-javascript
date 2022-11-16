8678bbb01e1d5928b799f4f3445a822c8e90f394
Aline Silva <aline.bsbsouza@gmail.com>
2022-11-16 15:39:13 +0000

    update

---
+++ exercicios/07objetos.js
@@ -0,0 +1,6 @@
+/*
+1 - Crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor, e um gasto médio de combustível por km rodado.
+
+Crie um método que dado a quantidade de km e preço do combustível nos dê o valor gasto em reais para realizar este percurso.
+*/
