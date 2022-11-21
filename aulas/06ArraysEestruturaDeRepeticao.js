f96a3372486d0f91bcdbc77336f2637233711350
Aline Silva <aline.bsbsouza@gmail.com>
2022-11-21 16:54:00 +0000

    up

---
+++ aulas/06ArraysEestruturaDeRepeticao.js
@@ -0,0 +1,17 @@
+/*const aluno = ['joao','vitor', 'marina'];
+
+aluno.push('Aline') //adicionar dinamicamente
+aluno[1] = 'Vinicius' //aicionar diretamente a posição
+
+
+console.log(aluno) */
+
+const notas = [];
+
+notas.push(5);
+notas.push(5);
+notas.push(5);
+notas.push(5);
+notas.push(2);
+
+console.log(notas.length);
