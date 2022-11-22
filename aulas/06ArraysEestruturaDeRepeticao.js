ab3f6e817251f02c413122e498915442ae331eb7
Aline Silva <aline.bsbsouza@gmail.com>
2022-11-22 12:35:38 +0000

    up

--- aulas/06ArraysEestruturaDeRepeticao.js
+++ aulas/06ArraysEestruturaDeRepeticao.js
@@ -6,7 +6,7 @@ aluno[1] = 'Vinicius' //aicionar diretamente a posição
 
 console.log(aluno) */
 
-const notas = [];
+/*const notas = [];
 
 notas.push(5);
 notas.push(5);
@@ -15,3 +15,29 @@ notas.push(5);
 notas.push(2);
 
 console.log(notas.length);
+*/
+
+/*const nome = 'Aline'
+
+for(let i = 0; i<nome.length; i++){ // variavel; condiçao; incremento;
+    console.log(nome[i]);
+} */
+
+const notas =[]
+
+notas.push(5)
+notas.push(6)
+notas.push(8)
+notas.push(5)
+notas.push(5)
+notas.push(5)
+
+let soma = 0
+
+for (let i = 0; i <notas.length; i++) {
+    const nota = notas[i];
+    soma = soma +nota
+}
+
+const media = soma / notas.length
+console.log(media.toFixed(2))
