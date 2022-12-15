aaaa825d38c71dcb223eb65aa4303a7513651418
Aline Silva <aline.bsbsouza@gmail.com>
2022-12-15 15:35:24 +0000

    up

---
+++ aulas/07loops.js
@@ -0,0 +1,22 @@
+
+ let numero = prompt("Digite um numero")
+ let indice = 0
+
+ while(indice <=10){
+     document.write(`${indice} x ${numero} = ${(indice * numero)} <br>`)
+     indice++
+ }
+     document.write("Saiu do loop while")
+
+
+
+ for(let i = 0; i<= 10; i++){
+     document.write(`${numero} x ${i} = ${(i * numero)} <br>`)
+ }
+     document.write("Saiu do loop For")
+
+
+ do{
+
+} while()
+
